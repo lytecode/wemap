f349b6d1d397a4f65d85b7280d0aefa13357dc96
lytecode <mbonubasil@gmail.com>
2021-07-29 12:51:05 +0100

    Inital commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`App started on port ${PORT}`))
