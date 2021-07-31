1c6740f48a51db551832a5b2a22d85a6668dcfbe
lytecode <mbonubasil@gmail.com>
2021-07-31 17:10:48 +0100

    Add routes to index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { db } from "./database/connect";
+import routes from "./routes";
+
+//Start database
+db().catch((err) =>
+  console.log("Ooops! Something went wrong with db connection")
+);
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log(`App started on port ${PORT}`))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//api routes
+app.use("/api/v1", routes);
+
+// Setup a default catch-all route
+app.use("*", (req, res, next) => {
+  res.status(404).json({
+    error: "Invalid route!",
+  });
+  next();
+});
+
+app.listen(PORT, () => console.log(`App started on port ${PORT}`));
