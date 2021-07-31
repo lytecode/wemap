83239b93604833cd61cf2df01ecf11e64d2642d1
lytecode <mbonubasil@gmail.com>
2021-07-31 21:00:26 +0100

    Add address router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import UserController from "../users/user.controller";
 import AuthController from "../auth/auth.controller";
+import AddressController from "../address/address.controller";
 
 const app = Router();
 
@@ -10,5 +11,7 @@ app.get("/", (req, res) => {
 
 app.post("/auth/login", AuthController.userLogin);
 app.post("/users", UserController.createUser);
+app.post("/addresses/verify", AddressController.getAddress);
+app.post("/addresses/weather", AddressController.checkWeather);
 
 export default app;
