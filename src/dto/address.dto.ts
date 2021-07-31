8766113d26f23439713d535c74c2361e44699fe6
lytecode <mbonubasil@gmail.com>
2021-07-31 21:02:13 +0100

    Address dto

---
+++ src/dto/address.dto.ts
@@ -0,0 +1,9 @@
+export interface IAddress {
+  street?: string;
+  streetNumber?: number;
+  town?: string;
+  postalCode?: string;
+  country?: string;
+  latitude?: number;
+  longitude?: number;
+}
