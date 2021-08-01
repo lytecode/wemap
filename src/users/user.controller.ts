6422a855aeb4ac5187c8a553ae3f49d4a306360d
lytecode <mbonubasil@gmail.com>
2021-08-01 14:05:37 +0100

    Add user login method

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -7,6 +7,17 @@ class UserController {
     if (user.error) return res.status(400).json({ error: user.error });
     res.status(201).json({ msg: "User created", data: user });
   }
+
+  async userLogin(req: Request, res: Response) {
+    const user = await UserService.signInUser(req.body);
+    if (user.error) return res.status(401).json({ error: user.error });
+    if (user.internalError)
+      return res.status(500).json({ error: user.internalError });
+
+    return res
+      .status(200)
+      .json({ msg: "Successfully signed in", token: user.token });
+  }
 }
 
 export default new UserController();
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,4 +1,6 @@
 import argon2 from "argon2";
+import config from "../config";
+import jwt from "jsonwebtoken";
 import { UserModel, IUser } from "./user.model";
 import { validateEmail } from "../utils/emailValidation";
 
@@ -18,6 +20,37 @@ class UserService {
     } catch (error) {
       return { error: "User with email already exist" };
     }
+  }
+
+  async signInUser(loginDTO: {
+    email: string;
+    password: string;
+  }): Promise<{ token?: string; error?: string; internalError?: string }> {
+    const { email, password } = loginDTO;
+    const user = await UserModel.findOne({ email });
+
+    if (!user) return { error: "Incorrect email or password" };
+    //compare password
+    try {
+      if (!(await argon2.verify(user.password, password))) {
+        return { error: "Incorrect email or password" };
+      }
+    } catch (err) {
+      return { internalError: "Cannot login at the moment, please try again" };
+    }
+
+    //password matched
+    const payload = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
+
+    const token = jwt.sign(payload, `${config.JWT_SECRET}`, {
+      expiresIn: config.TOKEN_EXPIRES_IN,
+    });
+
+    return { token };
   }
 }
 
