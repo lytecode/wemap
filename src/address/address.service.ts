d2bbbdc72d8b672ef4d95fbb1f56b7996b27abc1
lytecode <mbonubasil@gmail.com>
2021-08-01 20:52:38 +0100

    Cache external api request with redis

--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -1,12 +1,19 @@
 import axios from "axios";
 import { IAddress } from "../dto/address.dto";
 import config from "../config";
+import redis from "../utils/redis";
 
 class AddressService {
   async validateAddress(addressDTO: IAddress) {
     const { street, streetNumber, town, postalCode, country } = addressDTO;
 
+    //check redis cache for data
+    const cachedEntry = await redis.get(`address:${street}-${country}`);
+    //return cached if it exist
+    if (cachedEntry) return JSON.parse(cachedEntry);
+
     try {
+      //if cache miss, make request to api
       const { data } = await axios.get(
         `${config.NOMINATIM_BASE_API}/search.php?`,
         {
@@ -18,6 +25,14 @@ class AddressService {
             format: "json",
           },
         }
+      );
+
+      //store result in redis
+      redis.set(
+        `address:${street}-${country}`,
+        JSON.stringify(data),
+        "EX",
+        43200
       );
 
       return data;
@@ -32,32 +47,43 @@ class AddressService {
   async getWeather(addressDTO: IAddress) {
     const { street, streetNumber, town, postalCode, country } = addressDTO;
     let data;
-    try {
-      const result = await axios.get(
-        `${config.NOMINATIM_BASE_API}/search.php?`,
-        {
-          params: {
-            street: `${streetNumber}, ${street}`,
-            city: town,
-            postalcode: postalCode,
-            country,
-            format: "json",
-          },
-        }
-      );
-      data = result.data;
-    } catch (error) {
-      return {
-        error:
-          "Unable to complete request at this time, please try again later",
-      };
-    }
+    //check redis cache for data
+    const cachedEntry = await redis.get(`address:${street}-${country}`);
+    //return cached if it exist
+    if (!cachedEntry) {
+      try {
+        const result = await axios.get(
+          `${config.NOMINATIM_BASE_API}/search.php?`,
+          {
+            params: {
+              street: `${streetNumber}, ${street}`,
+              city: town,
+              postalcode: postalCode,
+              country,
+              format: "json",
+            },
+          }
+        );
+        data = result.data;
 
-    if (data.length === 0) {
-      return { msg: "Address could not be found" };
+        if (data.length === 0) {
+          return { msg: "Address could not be found" };
+        }
+      } catch (error) {
+        return {
+          error:
+            "Unable to complete request at this time, please try again later",
+        };
+      }
     }
+    //we have data in cach for this address
+    data = JSON.parse(cachedEntry as string);
 
     const { lat, lon } = data[0];
+
+    //check if we have entry for this lat and lon
+    const weatherEntry = await redis.get(`weather:${lat}-${lon}`);
+    if (weatherEntry) return JSON.parse(weatherEntry);
 
     try {
       const { data: weather } = await axios.get(`${config.TIMER7_BASE_API}?`, {
@@ -68,6 +94,9 @@ class AddressService {
           output: "json",
         },
       });
+
+      //cache data in redis
+      redis.set(`weather:${lat}-${lon}`, JSON.stringify(weather), "EX", 43200);
 
       return weather;
     } catch (error) {
