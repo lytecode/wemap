64df95e6069d47dbd2246b9944aab25650df0cbd
lytecode <mbonubasil@gmail.com>
2021-08-01 03:34:35 +0100

    Secure weather route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,21 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import UserController from "../users/user.controller";
 import AuthController from "../auth/auth.controller";
 import AddressController from "../address/address.controller";
+import auth from "../middleware/authentication";
 
 const app = Router();
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.json("Welcome to Weather Api!");
 });
 
 app.post("/auth/login", AuthController.userLogin);
 app.post("/users", UserController.createUser);
 app.post("/addresses/verify", AddressController.getAddress);
+
+//secured routes
+app.use(auth);
 app.post("/addresses/weather", AddressController.checkWeather);
 
 export default app;
