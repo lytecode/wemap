148750551aba6d3a8a98fa878ec0edd8e2705c4a
lytecode <mbonubasil@gmail.com>
2021-08-01 12:24:00 +0100

    Seup swagger docs

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import { db } from "./database/connect";
 import routes from "./routes";
 import config from "./config";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 
 //Start database
 db().catch((err) =>
@@ -15,6 +17,26 @@ const PORT = config.PORT;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//setup swagger docs
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      version: "1.0.0",
+      title: "Address verification and Weather Service",
+      description: "API for weather updates and address verification",
+      license: {
+        name: "MIT",
+        url: "https://opensource.org/licenses/MIT",
+      },
+    },
+    servers: [{ url: `http://localhost:${config.PORT}` }],
+  },
+  apis: ["./src/routes/index.ts"],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 //api routes
 app.use("/api/v1", routes);
